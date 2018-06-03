d31d9a623bbf42e89636af2ce596acb6dc04510b
aman9463 <aman9463@gmail.com>
2018-06-03 18:25:25 +0530

    slug added

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -66,7 +66,7 @@ class RoomsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_room
-      @room = Room.find(params[:id])
+      @room = Room.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,12 @@
 class Room < ApplicationRecord
+
+		extend FriendlyId
+	friendly_id :slug_candidates, use: [:slugged, :finders]
+
+	def slug_candidates
+    [
+      :name,
+      [:name]
+    ]
+  end
 end
