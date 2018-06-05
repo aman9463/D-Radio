dc9826c8b6c7839c807b391e237472f917ed1d1e
aman9463 <aman9463@gmail.com>
2018-06-05 17:37:26 +0530

    auth changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+	  def authenticate_user!(options={})
+    if user_signed_in?
+      super(options)
+    else
+      redirect_to root_path, alert: 'Pleas login first' ## if you want render 404 page
+      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+    end
+  end
 end
