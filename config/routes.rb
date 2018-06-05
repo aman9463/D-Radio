07845f9956742e928b35d9018a53bdd28231119e
aman9463 <aman9463@gmail.com>
2018-06-05 17:56:24 +0530

    img compress

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 	root 'rooms#index'
   resources :rooms
  scope module: :users do
-        devise_for :users
+        devise_for :users, skip: [:passwords, :passwords, :registrations]
       end
 devise_scope :user do
       get 'auth/login', to: 'users/sessions#custom_login', as: :login
