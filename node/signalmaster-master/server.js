97aaa6bf1c40e2b4053bfda913724405d5f40f9d
aman9463 <aman9463@gmail.com>
2018-06-05 19:54:35 +0530

    forman

--- node/signalmaster-master/server.js
+++ node/signalmaster-master/server.js
@@ -35,8 +35,8 @@ if (config.uid) process.setuid(config.uid);
 
 var httpUrl;
 if (config.server.secure) {
-    httpUrl = "http://165.227.225.85:" + port;
+    httpUrl = "http://dradio.rjght.com:" + port;
 } else {
-    httpUrl = "http://165.227.225.85:" + port;
+    httpUrl = "http://dradio.rjght.com:" + port;
 }
 console.log(yetify.logo() + ' -- signal master is running at: ' + httpUrl);
