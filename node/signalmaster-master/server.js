6bc00db936b5f09713d6290a6b0bb0f980c85ff7
aman9463 <aman9463@gmail.com>
2018-06-06 20:00:13 +0530

    serve js changes

--- node/signalmaster-master/server.js
+++ node/signalmaster-master/server.js
@@ -37,6 +37,6 @@ var httpUrl;
 if (config.server.secure) {
     httpUrl = "https://dradio.rjght.com" + port;
 } else {
-    httpUrl = "https://dradio.rjght.com:" + port;
+    httpUrl = "https://dradio.rjght.com";
 }
 console.log(yetify.logo() + ' -- signal master is running at: ' + httpUrl);
