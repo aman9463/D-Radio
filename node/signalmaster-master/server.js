76d59a8a0e61569a46fa46645fb0a65e39571514
aman9463 <aman9463@gmail.com>
2018-06-08 17:31:13 +0530

    node changes

--- node/signalmaster-master/server.js
+++ node/signalmaster-master/server.js
@@ -3,8 +3,7 @@ var yetify = require('yetify'),
     config = require('getconfig'),
     fs = require('fs'),
     sockets = require('./sockets'),
-    // port = parseInt(process.env.PORT || config.server.port, 10),
-    port = config.server.port,
+    port = parseInt(process.env.PORT || config.server.port, 10),
     server_handler = function (req, res) {
         if (req.url === '/healthcheck') {
             console.log(Date.now(), 'healthcheck');
@@ -18,15 +17,15 @@ var yetify = require('yetify'),
     server = null;
 
 // Create an http(s) server instance to that socket.io can listen to
-// if (config.server.secure) {
-//     server = require('https').Server({
-//         key: fs.readFileSync(config.server.key),
-//         cert: fs.readFileSync(config.server.cert),
-//         passphrase: config.server.password
-//     }, server_handler);
-// } else {
+if (config.server.secure) {
+    server = require('https').Server({
+        key: fs.readFileSync(config.server.key),
+        cert: fs.readFileSync(config.server.cert),
+        passphrase: config.server.password
+    }, server_handler);
+} else {
     server = require('http').Server(server_handler);
-// }
+}
 server.listen(port);
 
 sockets(server, config);
@@ -35,8 +34,8 @@ if (config.uid) process.setuid(config.uid);
 
 var httpUrl;
 if (config.server.secure) {
-    httpUrl = "http://dradio.rjght.com" + port;
+    httpUrl = "https://dradio.rjght.com:" + port;
 } else {
-    httpUrl = "https://dradio.rjght.com:" + 8888;
+    httpUrl = "https://dradio.rjght.com:" + port;
 }
 console.log(yetify.logo() + ' -- signal master is running at: ' + httpUrl);
